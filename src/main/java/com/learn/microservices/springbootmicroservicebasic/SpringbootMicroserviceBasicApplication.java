0635de8cd1d53e0c866ef86e15a2b4542501b447
KarthikDev <clickforkarthik@gmail.com>
2020-08-23 11:44:45 +0200

    First Commit with Welcome Controller

---
+++ src/main/java/com/learn/microservices/springbootmicroservicebasic/SpringbootMicroserviceBasicApplication.java
@@ -0,0 +1,13 @@
+package com.learn.microservices.springbootmicroservicebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/microservices/springbootmicroservicebasic/SpringbootMicroserviceBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.microservices.springbootmicroservicebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
